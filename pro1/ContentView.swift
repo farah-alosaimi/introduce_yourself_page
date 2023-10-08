2d65ed27d377af302290bb5d0d99d2edded67f58
farah-alosaimi <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>
2023-10-08 12:04:03 +0300

    t

--- pro1/ContentView.swift
+++ pro1/ContentView.swift
@@ -1,6 +1,7 @@
+
 //
 //  ContentView.swift
-//  pro1
+//  test1
 //
 //  Created by Farah Hasan Alosaimi on 23/03/1445 AH.
 //
@@ -9,15 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemPink)
+                .opacity(0.3)
+                .ignoresSafeArea()
+            
+            
+            VStack{
+                Image("PHOTO")
+                    .resizable()
+                    .frame(width: 300, height: 300) // Increase the size of the image
+                    .clipShape(Circle()) // Make the image circular
+                    .overlay(
+                        Circle()
+                            .stroke(Color.pink, lineWidth: 3)
+                    )
+                    .scaledToFit()
+                Text("Farah Alosaimi")
+                //  .fixedSize()
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.pink)
+                Text("\nMotivated Artificial Intelligence engineer, with experience in machine learning, deep learning, natural language processing, computer vision, and data analysis. Enjoy working in a team environment and independently, passionate about finding solutions to AI problems, and data analysis. Committed to quality and efficiency.")
+                
+            }
+            
         }
-        .padding()
-    }
-}
+        }
+        }
+    
+
 
 #Preview {
     ContentView()
