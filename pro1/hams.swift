203e95c8b6882e60f6496ea58e0a26c0df6a134b
Hams Alzahrani <hamsmohsen692@gmail.com>
2023-10-08 13:59:11 +0300

    hams

---
+++ pro1/hams.swift
@@ -0,0 +1,18 @@
+//
+//  hams.swift
+//  pro1
+//
+//  Created by Hams Alzahrani on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct hams: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    hams()
+}
