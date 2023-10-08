852f47e805272ca9883c80fe017cba1e82aa87f6
farah-alosaimi <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>
2023-10-08 11:50:28 +0300

    Initial Commit

---
+++ pro1/pro1App.swift
@@ -0,0 +1,17 @@
+//
+//  pro1App.swift
+//  pro1
+//
+//  Created by Farah Hasan Alosaimi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct pro1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
